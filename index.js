8fa53f22916134442c66d75e3370204e05d77f53
Elijah <ekollie324@gmail.com>
2023-09-30 00:15:34 -0400

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+// wrapGifts(gifts);
+
+
+
+
+function writeCards(names, giftType = `gift!`){
+   if (giftType != `gift!`){
+    giftType += ` gift!`;
+   } 
+    for (let i = 0; i < names.length; i++){
+       names[i] = (`Thank you, ${names[i]}, for the wonderful ${giftType}`);      
+    }
+    return names;
+    ;
+}
+
+function countDown(i){
+    for (i; i >= 0; i--){        
+        console.log(i);
+    }
+}
+function countUp(i, ){
+    while( i <= 10){
+         console.log(i);
+         i++
+    }
+}
